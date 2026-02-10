fa62bf95683858b6ba99732ebd642d5f2ea20545
the-maus <msampaio.mail@gmail.com>
2026-02-10 18:11:31 -0300

    model/migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "HT MGMT";
+Route::get('/email', function () {
+    Mail::raw('Test message', function(Message $message){
+        $message->to('test@gmail.com')
+                ->subject('Welcome to HR MGMT')
+                ->from('hr@hrmgmt.com');
+    });
+
+    echo "Mail sent!";
 });
